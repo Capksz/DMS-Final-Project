a99099340908902d29a1e1a9186f1bbb92d419e4
Capksz <emosker@gmail.com>
2024-12-14 15:43:34 -0500

    login stuff

--- app/app.py
+++ app/app.py
@@ -1,12 +1,14 @@
 import os
 import sqlite3
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify, render_template, session
 from flask_sqlalchemy import SQLAlchemy
 from models import db, User
 from prepopulate_db import prepopulate_db
 
 
+
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)  
@@ -17,9 +19,24 @@ def home():
     return "Hello, Flask!"
 
 # login
-@app.route('/login')
+@app.route('/login', methods=['GET'])
+def login_form():
+    return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
 def login():
-    pass
+    """Authenticate user by username and password."""
+    data = request.form
+    username = data.get('username')
+    password = data.get('password')
+
+    user = User.query.filter_by(username=username).first()
+    if user and user.check_password(password):
+        session['username'] = user.username
+        session['role'] = user.role
+        return jsonify({"message": "Login successful!", "user_id": user.role}), 200
+    else:
+        return jsonify({"message": "Invalid username or password"}), 401
 
 # publish game
 @app.route('/publish')
--- app/prepopulate_db.py
+++ app/prepopulate_db.py
@@ -3,12 +3,15 @@ from models import db, User
 def prepopulate_db(app):
     """Populate the database with initial data."""
     with app.app_context():
-        if User.query.first() is None:  # Check if the User table is empty
+        if User.query.first() is None:
             print("Prepopulating the database...")
             user1 = User(username="John Doe", email="john.doe@example.com")
+            user1.set_password("test")
             user2 = User(username="Jane Smith", email="jane.smith@example.com")
+            user2.set_password("test2")
             user3 = User(username="Alice Johnson", email="alice.johnson@example.com")
-            db.session.add_all([user1, user2, user3])  # Add multiple users
+            user3.set_password("test3")
+            db.session.add_all([user1, user2, user3])
             db.session.commit()
             print("Database prepopulated successfully!")
         else:
