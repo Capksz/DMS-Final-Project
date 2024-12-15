482c495eedebcc4d4dc19f7755effa5ad1bfcac2
Capksz <emosker@gmail.com>
2024-12-15 02:12:26 -0500

    made changes to models.py to better reflect game

--- app/models.py
+++ app/models.py
@@ -1,6 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
-from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
@@ -27,16 +26,37 @@ class Game(db.Model):
     developer = db.Column(db.String(100))
     publisher = db.Column(db.String(100))
     release_date = db.Column(db.Date)
+    required_age = db.Column(db.Integer, nullable=True, default=0)
+    dlc_count = db.Column(db.Integer, nullable=True, default=0)
+    achievements = db.Column(db.Integer, nullable=True, default=0)
+    windows_support = db.Column(db.Boolean, nullable=True, default=False)
+    mac_support = db.Column(db.Boolean, nullable=True, default=False)
+    linux_support = db.Column(db.Boolean, nullable=True, default=False)
+    supported_languages = db.Column(db.Text, nullable=True, default="")
+    full_audio_languages = db.Column(db.Text, nullable=True, default="")
+    categories = db.Column(db.Text, nullable=True, default="")
+    genres = db.Column(db.Text, nullable=True, default="")
+    game_tags = db.Column(db.Text, nullable=True, default="")
+    def get_supported_languages(self):
+        return self.supported_languages.split(',')
+
+    def get_categories(self):
+        return self.categories.split(',')
+
+    def set_supported_languages(self, languages_list):
+        self.supported_languages = ','.join(languages_list)
+
+    def set_categories(self, categories_list):
+        self.categories = ','.join(categories_list)
 
 
 class LibraryGame(db.Model):  # weak entity dependent on user and game
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    game_id = db.Column(db.Integer, db.ForeignKey('game.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+    game_id = db.Column(db.Integer, db.ForeignKey('game.id'), primary_key=True)
     last_played = db.Column(db.DateTime)
     favorite_status = db.Column(db.Boolean, default=False)
     hours_played = db.Column(db.Numeric(6, 2), default=0.0)
     is_downloaded = db.Column(db.Boolean, default=False)
-    game = db.relationship('Game', backref='library_entries')
 
 
 class Payment(db.Model):
